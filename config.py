6ea3eb2a36ada3f2cc2070803ad9b07a7a573e5d
budana12 <sirowabudana@gmail.com>
2025-05-14 22:04:40 +0530

    config.py

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = "7472777466:AAHdCsYtZpPnoMv67OL5_Hv2yjvy2Nz8jEg"
-    API_ID = 22581733
-    API_HASH = "1db7bdcf908100cc641c6a5276765c3d"
-    ADMIN = '6530997270'.split(',')
+    BOT_TOKEN = "7464498219:AAFacXLo-pvsNBWog8jgXtd3YU0pLukVvJ0"
+    API_ID = 10768857
+    API_HASH = "81b5d675d410f16ceb39df10db9cdb54"
+    ADMIN = '6520378417'.split(',')
     ADMIN_ID = [int(id) for id in ADMIN]
     DB_URL = "mongodb+srv://neoayush444:3kuwGf9jKicbDxvT@cluster0.f9vq7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     DB_NAME = "Crack_Jack"
-    TXT_LOG = -1002294532244
-    AUTH_LOG = -1002294532244
-    HIT_LOG = -1002294532244
+    TXT_LOG = -1002631081956
+    AUTH_LOG = -1002631081956
+    HIT_LOG = -1002631081956
     DRM_DUMP = -1002294532244
-    CHANNEL = --1002487777733
-    CH_URL = "https://t.me/justfortestinggwak"
-    OWNER = "https://t.me/mrcraacker"
-    THUMB_URL = "https://vault.pictures/media/images/b9/ad/12/b9ad123d95cd49465fcb954d.jpg" #Replace by with your Thumb URL
+    CHANNEL = --1002294532244
+    CH_URL = ""
+    OWNER = ""
+    THUMB_URL = "" #Replace by with your Thumb URL
     HOST = "https://drm-api-five.vercel.app"
